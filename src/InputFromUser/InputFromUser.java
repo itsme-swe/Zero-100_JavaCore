32a4b0817015e2444e69945f2d77ebbf55c6df44
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-16 13:34:05 +0530

    Initial Commit

---
+++ src/InputFromUser/InputFromUser.java
@@ -0,0 +1,24 @@
+package InputFromUser;
+
+import java.util.Scanner;
+
+public class InputFromUser {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.println("Enter two numbers: ");
+
+        a = s.nextInt();
+        b = s.nextInt();
+        c = a + b;
+
+        System.out.printf("The sum of %d and %d is " + c, a, b);
+    }
+}
+
+/*
+🔸Output: The sum of 10 and 20 is 30
+ */
