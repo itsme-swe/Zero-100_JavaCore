268f2a7e0116e6e3ebdd11f26256a9b63081c388
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-18 17:36:16 +0530

    Initial Commit

---
+++ src/InputOutputStreams/CreatingFileMethod2.java
@@ -0,0 +1,19 @@
+package InputOutputStreams;
+
+import java.io.FileOutputStream;
+
+public class CreatingFileMethod2 {
+    public static void main(String[] args) throws Exception {
+
+        try ( FileOutputStream fos = new FileOutputStream("E:/JavaDocs/Text2.txt");) {
+
+            String str1 = "Learning Core Java";
+
+            fos.write(str1.getBytes());
+        }
+    }
+}
+
+/*
+◽This is another method to create file using FileOutputStream. Here we are not using any try and catch block instead of using try block we are try-and-resources and inplace of catch block we are throwing excepting by using throws Exception.
+* */
